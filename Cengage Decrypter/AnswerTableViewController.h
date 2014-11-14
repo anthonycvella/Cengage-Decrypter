c0344e0511b05382b26a28cb44931f317bb682fb
Anthony Vella <anthonycvella@gmail.com>
2014-11-14 12:28:39 -0500

    Commit changes before push

---
+++ Cengage Decrypter/AnswerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnswerTableViewController.h
+//  Cengage Decrypter
+//
+//  Created by SuchyMac3 on 4/29/13.
+//  Copyright (c) 2013 ITP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerTableViewController : UITableViewController
+
+@end
